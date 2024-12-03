b26cbdfe3839bc1c6d427722c6b2f69a4bcd9b04
Mathieu_Prier <mathieu.priez@live.fr>
2024-12-03 13:30:27 +0100

    merge from dev

--- client/src/pages/Journal.tsx
+++ client/src/pages/Journal.tsx
@@ -2,11 +2,43 @@ import React from "react";
 import "./Journal.css";
 
 function Journal() {
-    return ( 
-        <>
-        h1 vous êtes dans le journal
-        </>
-     );
+    return (
+					<>
+						<h1>post-it</h1>
+						<button
+							id="creaPostIt"
+							className="icon-postit"
+							key="uniqueKey"
+							type="button"
+						>
+							un click pour un post-it ...
+						</button>
+						<button
+							id="creaPostIt"
+							className="icon-postit"
+							key="uniqueKey"
+							type="button"
+						>
+							un click pour un post-it ...
+						</button>
+						<button
+							id="creaPostIt"
+							className="icon-postit"
+							key="uniqueKey"
+							type="button"
+						>
+							un click pour un post-it ...
+						</button>
+						<button
+							id="creaPostIt"
+							className="icon-postit"
+							key="uniqueKey"
+							type="button"
+						>
+							un click pour un post-it ...
+						</button>
+					</>
+				);
 }
 
 export default Journal;
