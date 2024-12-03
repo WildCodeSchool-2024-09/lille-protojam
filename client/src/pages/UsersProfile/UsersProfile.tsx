d07d0a60102b2d04e558ff7e23bc195a237eb685
ALICIA <alicia.magro@gmail.com>
2024-12-03 15:28:12 +0100

    still have a few things to work on

--- client/src/pages/UsersProfile/UsersProfile.tsx
+++ client/src/pages/UsersProfile/UsersProfile.tsx
@@ -1,17 +1,24 @@
 import "./UsersProfile.css";
+import { Link } from "react-router-dom";
 
 function UsersProfile() {
 	return (
 		<>
 			<main className="profile-container">
 				<section className="action-container">
-					<p className="title-action">Action du jour</p>
+					<Link to="/profile-actions" className="link-profile">
+						<p className="title-profile">Action du jour</p>
+					</Link>
 				</section>
 				<section className="mood-container">
-					<p className="title-mood">Humeur du jour</p>
+					<Link to="/profile-mood" className="link-profile">
+						<p className="title-profile">Humeur du jour</p>
+					</Link>
 				</section>
 				<section className="journal-container">
-					<p className="title-journal">Journal de gratitude</p>
+					<Link to="/profile-journal" className="link-profile">
+						<p className="title-profile">Journal de gratitude</p>
+					</Link>
 				</section>
 			</main>
 		</>
