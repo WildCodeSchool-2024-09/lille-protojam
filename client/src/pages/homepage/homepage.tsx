3de1947531235741ba4dd2529d8f58295aa24d81
Thomas Lamisse <tom.lamisse@gmail.com>
2024-12-03 16:37:11 +0100

    homepage first version

---
+++ client/src/pages/homepage/homepage.tsx
@@ -0,0 +1,15 @@
+
+import './homepage.css';
+
+function Homepage() {
+  return (
+   <main> <div className='button-container' >
+      <button className='button-connexion'>Se connecter</button>
+    </div>
+    <h2>FEEL GOOD</h2>
+    <div className='citation-container'><h1 className='title'>Citation du jour</h1><p className='text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis laudantium facilis neque, est maxime laborum illo saepe dicta rerum, nisi explicabo consectetur laboriosam asperiores pariatur obcaecati, vel eveniet iste tenetur.</p></div>
+    </main>
+  );
+}
+
+export default Homepage;
