0a606687a2d98bfe9d5918c568a1e7c6779dfcfd
ALICIA <alicia.magro@gmail.com>
2024-12-04 11:02:12 +0100

    debuged the little mess

--- client/src/pages/UsersProfile/UsersProfile.tsx
+++ client/src/pages/UsersProfile/UsersProfile.tsx
@@ -5,17 +5,17 @@ function UsersProfile() {
 	return (
 		<>
 			<main className="profile-container">
-				<section className="action-container">
+				<section className="action-container-profile">
 					<Link to="/profile/actions" className="link-profile">
 						<p className="title-profile">Action du jour</p>
 					</Link>
 				</section>
-				<section className="mood-container">
+				<section className="mood-container-profile">
 					<Link to="/profile/mood" className="link-profile">
 						<p className="title-profile">Humeur du jour</p>
 					</Link>
 				</section>
-				<section className="journal-container">
+				<section className="journal-container-profile">
 					<Link to="/profile/journal" className="link-profile">
 						<p className="title-profile">Journal de gratitude</p>
 					</Link>
