366bc5b488da746a7a100618a155136a426b769a
Emeric Lesage <elesage.dev@gmail.com>
2024-12-04 15:38:20 +0100

    color login page

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -4,60 +4,62 @@ import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../services/UserContext";
 
 function Login() {
-	const [login, setLogin] = useState({ email: "", password: "" });
-	const [errorMessage, setErrorMessage] = useState("");
-	const navigate = useNavigate();
-	const { setUserConnected } = useContext(UserContext);
+  const [login, setLogin] = useState({ email: "", password: "" });
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+  const { setUserConnected } = useContext(UserContext);
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		if (
-			login.email === "legentiltoto@bienveillance.fr" &&
-			login.password === "devenirgentil"
-		) {
-			setUserConnected(true);
-			navigate("/profile");
-		} else if (login.email !== "legentiltoto@bienveillance.fr") {
-			setErrorMessage("Mauvais email");
-		} else if (login.password !== "devenirgentil") {
-			setErrorMessage("mauvais mot de passe");
-		}
-	};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (
+      login.email === "legentiltoto@bienveillance.fr" &&
+      login.password === "devenirgentil"
+    ) {
+      setUserConnected(true);
+      navigate("/profile");
+    } else if (login.email !== "legentiltoto@bienveillance.fr") {
+      setErrorMessage("Mauvais email");
+    } else if (login.password !== "devenirgentil") {
+      setErrorMessage("mauvais mot de passe");
+    }
+  };
 
-	return (
-		<div className="container">
-			<form action="submit" className="login-form" onSubmit={handleSubmit}>
-				<label htmlFor="email">
-					Mon email:
-					<input
-						type="email"
-						name="email"
-						id="email"
-						value={login.email}
-						onChange={(e) => {
-							setLogin({ ...login, email: e.target.value });
-						}}
-						placeholder="mon email ici"
-					/>
-				</label>
-				<label htmlFor="password">
-					Mot de passe:
-					<input
-						type="password"
-						name="password"
-						id="password"
-						value={login.password}
-						placeholder="*******"
-						onChange={(e) => {
-							setLogin({ ...login, password: e.target.value });
-						}}
-					/>
-				</label>
-				{errorMessage && <p className="error-message"> {errorMessage}</p>}
-				<button type="submit">Valider</button>
-			</form>
-		</div>
-	);
+  return (
+    <div className="container">
+      <form action="submit" className="login-form" onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          Mon email:
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={login.email}
+            onChange={(e) => {
+              setLogin({ ...login, email: e.target.value });
+            }}
+            placeholder="mon email ici"
+          />
+        </label>
+        <label htmlFor="password">
+          Mot de passe:
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={login.password}
+            placeholder="*******"
+            onChange={(e) => {
+              setLogin({ ...login, password: e.target.value });
+            }}
+          />
+        </label>
+        {errorMessage && <p className="error-message"> {errorMessage}</p>}
+        <button type="submit" className="connected-button">
+          Valider
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
