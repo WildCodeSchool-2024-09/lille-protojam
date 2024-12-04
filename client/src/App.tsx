f8101a01ca1919d7ed784344e7deb7be89f6ee90
Arnaud6216 <guevaer8@gmail.com>
2024-12-04 15:25:24 +0100

    fixed board title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import { MoodProvider } from "./services/MoodContext";
+import { UserProvider } from "./services/UserContext";
 import { CounterProvider } from "./services/CounterContext";
 
 function App() {
