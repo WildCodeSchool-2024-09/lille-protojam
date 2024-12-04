5604e7f0a4529952e466c34acd7a0b97daa4bae9
Emeric Lesage <elesage.dev@gmail.com>
2024-12-04 15:18:15 +0100

    fix userprovider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,22 +3,21 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import { MoodProvider } from "./services/MoodContext";
 import { CounterProvider } from "./services/CounterContext";
+import { UserProvider } from "./services/UserContext";
 
 function App() {
   return (
     <>
-     <UserProvider>
-      <MoodProvider>
-        <CounterProvider>
-          <Navbar />
-          <Outlet />
-        </CounterProvider>
-      </MoodProvider>
-     </UserProvider>
-
+      <UserProvider>
+        <MoodProvider>
+          <CounterProvider>
+            <Navbar />
+            <Outlet />
+          </CounterProvider>
+        </MoodProvider>
+      </UserProvider>
     </>
   );
-
 }
 
 export default App;
